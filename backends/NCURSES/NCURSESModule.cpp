ae546a6cef3461f0964d24a30864c5c5fb608ee0
Hugo <hugo.cathelain@epitech.eu>
2025-03-20 18:46:42 +0100

    [ADD] center the game on the terminal and add little border that contains the map

--- backends/NCURSES/NCURSESModule.cpp
+++ backends/NCURSES/NCURSESModule.cpp
@@ -23,8 +23,19 @@ NCURSESModule::NCURSESModule(void)
     nodelay(stdscr, TRUE);
     timeout(0);
 
-    // y x oy ox
-    mWindow = subwin(stdscr, 31, 28 * 2, 0, 0);
+    int maxY, maxX;
+    getmaxyx(stdscr, maxY, maxX);
+
+    int windowHeight = 33;
+    int windowWidth = 29 * 2;
+    int startY = (maxY - windowHeight - 2) / 2;
+    int startX = (maxX - windowWidth - 2) / 2;
+
+    // Ensure window position is not negative
+    startY = (startY < 0) ? 0 : startY;
+    startX = (startX < 0) ? 0 : startX;
+
+    mWindow = subwin(stdscr, windowHeight, windowWidth, startY, startX);
 
     if (mWindow == nullptr) {
         endwin();
@@ -56,9 +67,9 @@ void NCURSESModule::Render(void)
     while (!API::IsDrawQueueEmpty()) {
         auto draw = API::PopDraw();
         auto [asset, x, y] = draw;
-        mvwprintw(mWindow, y, x * 2, asset.characters.c_str());
+        mvwprintw(mWindow, y + 1, (x * 2) + 1, asset.characters.c_str());
     }
-    //box(mWindow, ACS_VLINE, ACS_HLINE);
+    box(mWindow, ACS_VLINE, ACS_HLINE);
     refresh();
     wrefresh(mWindow);
 }
