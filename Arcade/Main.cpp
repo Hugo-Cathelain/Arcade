2e6976c3ea5e03514efcce312daa7e2a764010b6
mallory-scotton <mallory.scotton@epitech.eu>
2025-04-12 18:42:41 +0200

    add: adding new arguments to handle fast debug

--- Arcade/Main.cpp
+++ Arcade/Main.cpp
@@ -8,13 +8,19 @@
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <library>" << std::endl;
+    if (argc < 2 || argc > 3) {
+        std::cout << "Usage: " << argv[0] << " <library> [game]" << std::endl;
         return (84);
     }
 
+    std::string game = "lib/arcade_gui_menu.so";
+
+    if (argc == 3) {
+        game = argv[2];
+    }
+
     try {
-        Arc::Core core(argv[1], "lib/arcade_gui_menu.so");
+        Arc::Core core(argv[1], game);
         core.Run();
     } catch (const Arc::DLError& error) {
         std::cerr << error.what() << std::endl;
