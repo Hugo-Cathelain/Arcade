d44813e253fe6b4b8b3a5086e6136e6358728930
Hugo <hugo.cathelain@epitech.eu>
2025-04-14 17:20:56 +0200

    [ADD] new map for the nibbler

--- games/NIBBLER/Maps/Map.hpp
+++ games/NIBBLER/Maps/Map.hpp
@@ -9,6 +9,7 @@
 #include "../../../Arcade/utils/Vec2.hpp"
 #include "games/NIBBLER/Assets.hpp"
 #include "games/NIBBLER/Maps/Map01.hpp"
+#include "games/NIBBLER/Maps/Map02.hpp"
 #include <vector>
 
 namespace Arc::Nibbler
@@ -16,11 +17,13 @@ namespace Arc::Nibbler
 
 static const std::vector<std::vector<std::vector<SpriteType>>> MAPS = {
     MAP_01,
+    MAP_02,
     // Add more maps here
 };
 
 static const std::vector<std::vector<Vec2i>> MAPS_FRUIT = {
     MAP_01_FRUIT,
+    MAP_02_FRUIT,
     // Add more fruit positions here
 };
 
